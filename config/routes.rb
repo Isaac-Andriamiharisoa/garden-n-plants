64f9602f2aec77afe6254b169c30325217bf82b6
Dylan Therese <dylantherese@outlook.com>
2024-12-07 11:43:18 +0400

    home page created

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   # get "up" => "rails/health#show", as: :rails_health_check
 
+  root 'pages#home'
   get "/plants", to: "plants#index", as: :plants
   get "/plants/:id", to: 'plants#show', as: :plant
 
